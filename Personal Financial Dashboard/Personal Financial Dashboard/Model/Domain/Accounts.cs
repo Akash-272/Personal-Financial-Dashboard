eea36d507124c4828a879dbd982e24b0a6c43ebc
Akash Nalawade <analawade@blackmountainsystems.local>
2024-10-09 14:27:36 +0530

    first

---
+++ Personal Financial Dashboard/Personal Financial Dashboard/Model/Domain/Accounts.cs
@@ -0,0 +1,14 @@
+﻿namespace Personal_Financial_Dashboard.Model.Domain
+{
+    public class Accounts
+    {
+        public Guid AccountId { get; set; }
+        public Guid UderId { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public int Balance { get; set; }
+
+        public DateTime datetime { get; set; }
+
+    }
+}
---
+++ Personal Financial Dashboard/Personal Financial Dashboard/Model/Domain/Dto/LoginRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Personal_Financial_Dashboard.Model.Dto
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType (DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
